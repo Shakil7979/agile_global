64fc1bfceed9ef9ba050a77dc33d9cedf320995c
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-09-08 19:16:11 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,6 +15,17 @@ $(document).ready(function(){
 	// 	autoplay:true,
 	// });
 
+	$('.header_area').hover(
+        function() {
+            // Mouse enter - add class
+            $(this).addClass('header_white');
+        },
+        function() {
+            // Mouse leave - remove class
+            $(this).removeClass('header_white');
+        }
+    );
+
 
 
 
