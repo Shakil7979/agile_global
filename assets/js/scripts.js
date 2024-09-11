2878b6494283bb7527fe715f2628466a63722d39
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-09-11 19:48:37 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -202,5 +202,6 @@ $(document).ready(function() {
 $(document).ready(function(){
 	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
 		$(this).toggleClass('open');
+		$('.mobiler_for_menu').slideToggle();
 	});
 });
